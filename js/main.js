63d6626a6b1eaf8e56dda8081a0a9dcacdd6c2e5
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2024-12-17 20:23:27 +0100

    updating to github

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-// IMPORT DATABAS
+// IMPORT DATABAS AND DISPLAY FUNCTION
 import {db} from "./db.js";
 import {displayMenu} from "./ui.js";
 
-
+// Change this when we have decided the menu
 displayMenu(db.drinks);
 displayMenu(db.pizzas);
-
-console.log(`Current cart: ${localStorage.getItem('cart')}`)
